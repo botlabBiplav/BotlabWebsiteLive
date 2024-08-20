e1814be80b11d900952b345cf1acea9de013d641
PM42-Work <raghuvansh.agarwal@botlabdynamics.com>
2024-08-20 09:29:33 +0530

    mobile optimisation and low end system fix attempt 2

--- app/layout.js
+++ app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <title>
-          BotLab Dynamics | India's Leading Drone Show Company
+          BotLab Dynamics | India&apos;s Leading Drone Show Company
         </title>
         
       </head>
