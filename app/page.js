ef71bef5031d72cd88800070f94ab8a00881151c
BiplavBotLab <biplav.mazumdar@botlabdynamics.com>
2024-10-22 12:12:23 +0530

    versal isr check

--- app/page.js
+++ app/page.js
@@ -11,48 +11,49 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const token = process.env.NEXT_PUBLIC_BEARER_TOKEN;
 // Fetch data from your API with ISR
 // here we are implemented the isr on-demand revalidation using webhook
-const fetchData = (endpoint) => {
-  return fetch(`${API_URL}${endpoint}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-    next: { tags: ['posts'] }, // Use tags for webhook integration
-    cache: 'no-store',
-  })
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error(`Failed to fetch ${endpoint}`);
-    }
-    return res.json();
-  })
-  .then((data) => data.data);
-};
+// const fetchData = (endpoint) => {
+//   return fetch(`${API_URL}${endpoint}`, {
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//       "Content-Type": "application/json",
+//     },
+//     next: { tags: ['posts'] }, // Use tags for webhook integration
+//     cache: 'no-store',
+//   })
+//   .then((res) => {
+//     if (!res.ok) {
+//       throw new Error(`Failed to fetch ${endpoint}`);
+//     }
+//     return res.json();
+//   })
+//   .then((data) => data.data);
+// };
 
 
-const fetchAppData = () => {
-  return Promise.all([
-    fetchData(endpointData.heroSection),
-    fetchData(endpointData.storyPage),
-  ])
-  .then(([hero, story]) => {
-    return { hero, story };
-  });
-};
+// const fetchAppData = () => {
+//   return Promise.all([
+//     fetchData(endpointData.heroSection),
+//     fetchData(endpointData.storyPage),
+//   ])
+//   .then(([hero, story]) => {
+//     return { hero, story };
+//   });
+// };
 
 export default function Home() {
-  return fetchAppData().then(({ hero, story }) => {
-    console.log(story, 'herosectiondata');
+  // return fetchAppData().then(({ hero, story }) => {
+  //   console.log(story, 'herosectiondata');
 
     return (
       <main>
         <div className="z-10">
-          <Background />
+          <h1 className="text-white text-2xl mt-20">This is working fine</h1>
+          {/* <Background />
           <Herocard getData={hero} />
-          <StoryPage getData={story} />
+          <StoryPage getData={story} /> */}
         </div>
       </main>
     );
-  });
+  // });
 }
 
